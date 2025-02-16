3dcc9ffd5c514d4f7e80c901f3a737fc742639fd
CLAUDIA CANDELAS OVIEDO <claudiacandelas2005@gmail.com>
2025-02-16 19:26:41 +0100

    Clase AdmBaseDatos

--- app/src/main/java/com/example/listviewsqlite/AdmBaseDatos.java
+++ app/src/main/java/com/example/listviewsqlite/AdmBaseDatos.java
@@ -1,5 +1,36 @@
 package com.example.listviewsqlite;
 
-public class AdmBaseDatos {
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AdmBaseDatos extends SQLiteOpenHelper {
+
+
+    public AdmBaseDatos(@Nullable Runnable context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super((Context) context, name, factory, version);
+    }
+
+    public AdmBaseDatos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public AdmBaseDatos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE canciones (id INT PRIMARY KEY, nombre TEXT, cantante TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/example/listviewsqlite/MainActivity.java
+++ app/src/main/java/com/example/listviewsqlite/MainActivity.java
@@ -16,7 +16,6 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    // Creamos variables
     private ListView lista;
     private TextView texto;
     private RadioButton radioButton_pulsado;
@@ -24,14 +23,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Establece el layout principal
         setContentView(R.layout.activity_main);
 
-        // Vinculamos las variables con el layout
         lista = findViewById(R.id.listView);
         texto = findViewById(R.id.texto_cambiante);
 
-        // Crea y rellena el ArrayList usando la clase Encapsulador
         ArrayList<Encapsulador> datos = new ArrayList<Encapsulador>();
         datos.add(new Encapsulador("SOPA DE VERDURAS", "10/10", false));
         datos.add(new Encapsulador("SOPA DE COCIDO", "10/10", false));
@@ -41,34 +37,24 @@ public class MainActivity extends AppCompatActivity {
         datos.add(new Encapsulador("SOPA DE CEBOLLA", "5/10", false));
         datos.add(new Encapsulador("SOPA DE AJO", "5/10", false));
 
-        // Configura el adaptador, sin él no se puede mostrar nada e instancia el Adaptador
         lista.setAdapter(new Adaptador(this, R.layout.entrada, datos) {
 
-            // Sobrescribe onEntrada() para llenarlo con lso datos
             @Override
             public void onEntrada(Object entrada, View view) {
                 if (entrada != null) {
-                    // Encuentra las vistas en entrada.xml y las vincula
                     TextView texto_superior_entrada = (TextView) view.findViewById(R.id.texto_titulo);
                     TextView texto_inferior_entrada = (TextView) view.findViewById(R.id.texto_datos);
                     RadioButton miRadio = (RadioButton) view.findViewById(R.id.boton);
 
-                    // Agrega los datos usando Encapsulador
                     texto_superior_entrada.setText(((Encapsulador) entrada).get_textoTitulo());
                     texto_inferior_entrada.setText(((Encapsulador) entrada).get_textoContenido());
 
-                    // Evento al pulsar cualquier radioButton
                     miRadio.setOnClickListener(new View.OnClickListener() {
-                        // Metodo obligatorio que debe sobrescribir
                         @Override
                         public void onClick(View v) {
-                            // Comprueba si otro botón se ha pulsado
                             if (radioButton_pulsado != null)
-                                // Si habia uno pulsado lo desmarca
                                 radioButton_pulsado.setChecked(false);
-                            // Actualiza radioButton_pulsado para que apunte a la nueva seleccionada
                             radioButton_pulsado = (RadioButton) v;
-                            // Cambia el texto
                             texto.setText("MARCADA UNA OPCIÓN");
                         }
                     });
@@ -77,21 +63,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Clase que encapsula los datos
     public static class Encapsulador {
         // Variables
         private String titulo;
         private String texto;
         private boolean dato1;
 
-        // COnstructor
         public Encapsulador(String textoTitulo, String textoContenido, boolean favorito) {
             this.titulo = textoTitulo;
             this.texto = textoContenido;
             this.dato1 = favorito;
         }
-
-        // Getters
 
         public String get_textoTitulo() {
             return titulo;
